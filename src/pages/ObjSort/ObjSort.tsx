e42b6f069a6fb496d3b59ef4bbbd3c7f2e4c1f33
Ethan Kletschke <bbruhingson@gmail.com>
2025-09-01 09:42:53 +0200

    Start documenting code; Remove empty style files

--- src/pages/ObjSort/ObjSort.tsx
+++ src/pages/ObjSort/ObjSort.tsx
@@ -3,18 +3,23 @@ import GoHomeBtn from "../../comps/GoHomeBtn/GoHomeBtn.tsx";
 import Header from "../../comps/Header/Header.tsx";
 import sortObject from "../../funcs/sortObjects.ts";
 
+// Object Sorting Component
 export default function ObjSort() {
   return (
     <main>
+      {/* Main header component */}
       <Header>
         Object Sort
       </Header>
 
+      {/* Main page body */}
       <article id="main-body">
-      
+
+        {/* Button to return to index route */}
         <GoHomeBtn />
       </article>
 
+      {/* Footer component */}
       <Footer />
     </main>
   );
