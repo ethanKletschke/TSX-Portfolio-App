881667ec57d77e0247732f129a839d1571801ef5
Ethan Kletschke <bbruhingson@gmail.com>
2025-10-21 09:06:45 +0200

    Documentation: Fix old documentation

--- src/funcs/validateEmail.ts
+++ src/funcs/validateEmail.ts
@@ -1,5 +1,5 @@
 export default function validateEmail(email: string): boolean {
-  // Declare a RegExp for emails (or localhost addresses)
+  // Declare a RegExp for emails only (no localhost)
   const re = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/gim;
 
   // Returns if the email is valid as per the RegExp
