533a22a6547422ec39b1956f2cdc13612b90030c
Ethan Kletschke <bbruhingson@gmail.com>
2025-11-04 06:49:23 +0200

    WIP: Start adding content for Stopwatch

--- src/pages/StopwatchPage/StopwatchPage.tsx
+++ src/pages/StopwatchPage/StopwatchPage.tsx
@@ -1,7 +1,11 @@
+import Header from "../../comps/Header/Header";
+
 export default function StopwatchPage() {
   return (
     <main>
-      
+      <Header>
+        Stopwatch Example
+      </Header>
     </main>
   );
 }
