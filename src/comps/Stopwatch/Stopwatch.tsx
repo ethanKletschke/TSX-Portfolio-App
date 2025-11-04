4d913721dd6a43b6ad85b6878e9aea95f01e6cab
Ethan Kletschke <bbruhingson@gmail.com>
2025-11-04 12:17:21 +0200

    Docs: Add comments to Stopwatch component

--- src/comps/Stopwatch/Stopwatch.tsx
+++ src/comps/Stopwatch/Stopwatch.tsx
@@ -2,33 +2,38 @@ import { useState, useRef } from "react";
 import styles from "./Stopwatch.module.css";
 
 export default function Stopwatch() {
+  // Create a ref that persists
   const startTimeRef = useRef<number | null>(null); 
   const [now, setNow] = useState<number>(Date.now());
 
-
+  // Updates the timer
   const update = () => {
-    setNow(Date.now()); 
+    setNow(Date.now()); // Triggers a UI update
     requestAnimationFrame(update);
   };
 
+  // Starts the timer
   const handleStart = () => {
     startTimeRef.current = Date.now();
     requestAnimationFrame(update);
   };
 
-  const handleLog = () => {
-
-  };
-
-  let secondsPassed = 0;
-
+  
+  let timeElapsed = 0;
+  
   if (startTimeRef.current !== null) {
-    secondsPassed = (now - startTimeRef.current) / 1000;
+    timeElapsed = (now - startTimeRef.current) / 1000;
   }
-
+  
+  const handleLog = () => {
+    // TODO -> Make a textarea and log to it using state
+    // WIP -> Temporary logging of the current time elapsed
+    console.debug(timeElapsed.toFixed(2));
+  };
+  
   return (
     <div className={styles.stopwatchWrapper}>
-      <p>Seconds passed: {secondsPassed.toFixed(4)}</p>
+      <p>Seconds passed: {timeElapsed.toFixed(2)} seconds</p>
       <button onClick={handleStart}>Start</button>
       <button onClick={handleLog}>Log</button>
     </div>
