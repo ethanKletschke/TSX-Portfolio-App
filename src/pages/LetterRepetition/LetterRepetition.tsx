972fa24cc3d9d38a54363883d9ff8aaabe68a38e
Ethan Kletschke <bbruhingson@gmail.com>
2025-11-09 16:28:50 +0200

    WIP: Start LetterRepetition Page

---
+++ src/pages/LetterRepetition/LetterRepetition.tsx
@@ -0,0 +1,19 @@
+import Header from "../../comps/Header/Header";
+
+export default function LetterRepetition() {
+  return (
+    <main>
+      <Header>
+        Letter Repitition
+      </Header>
+
+      <article id="main-body">
+        <form method="post">
+
+        </form>
+      </article>
+
+      <hr />
+    </main>
+  );
+}
