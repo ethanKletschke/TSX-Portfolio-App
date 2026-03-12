e01ac60e8c0e9d40b0f5822507d91f7e433beaae
Ethan Kletschke <bbruhingson@gmail.com>
2026-03-12 14:38:48 +0200

    feat: Start TempConvert React component

--- src/pages/TempConvert/TempConvert.tsx
+++ src/pages/TempConvert/TempConvert.tsx
@@ -1,7 +1,23 @@
-export default function TempConvert() {
-  return (
-    <>
+import { useState } from "react";
+import Page from "../../comps/general_UI/layout/Page/Page";
+import Temperatures from "../../funcs/Temperatures";
 
-    </>
+export default function TempConvert() {
+  const [celsius, setCelsius] = useState(0);
+  const [fahrenheit, setFahrenheit] = useState(0);
+  // const [kelvin, setKelvin] = useState(0);
+
+  function handleC_to_F_Submit() {
+
+  }
+
+  return (
+    <Page headerText="Temperature Conversions">
+      <form onSubmit={handleC_to_F_Submit}>
+        <fieldset>
+          <legend>Celsius to Fahrenheit</legend>
+        </fieldset>
+      </form>
+    </Page>
   );
 }
