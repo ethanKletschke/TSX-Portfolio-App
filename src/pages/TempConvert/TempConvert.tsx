9371c7f8b430e18356bb952b4e7db8b73b08cdeb
Ethan Kletschke <bbruhingson@gmail.com>
2026-03-13 09:19:37 +0200

    feat: finish TempConvert page prototype

--- src/pages/TempConvert/TempConvert.tsx
+++ src/pages/TempConvert/TempConvert.tsx
@@ -1,23 +1,54 @@
 import { useState } from "react";
+import Form from "../../comps/general_UI/Form/Form";
 import Page from "../../comps/general_UI/layout/Page/Page";
 import Temperatures from "../../funcs/Temperatures";
 
 export default function TempConvert() {
-  const [celsius, setCelsius] = useState(0);
-  const [fahrenheit, setFahrenheit] = useState(0);
-  // const [kelvin, setKelvin] = useState(0);
+  // State
+  const [celsius, setCelsius] = useState(0); // Celsius input
+  const [fahrenheit, setFahrenheit] = useState(0); // Fahrenheit output
 
-  function handleC_to_F_Submit() {
+  const handleCelsiusChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setCelsius(Number(e.target.value));
+  };
 
-  }
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    setFahrenheit(Temperatures.celsiusToFahrenheit(celsius));
+  };
 
   return (
     <Page headerText="Temperature Conversions">
-      <form onSubmit={handleC_to_F_Submit}>
+      <Form submitHandler={handleSubmit}>
         <fieldset>
           <legend>Celsius to Fahrenheit</legend>
+
+          <label htmlFor="cels">
+            Celsius
+          </label>
+          <input
+            type="number"
+            id="cels"
+            name="cels"
+            value={celsius}
+            onChange={handleCelsiusChange}
+          />
+
+          <label htmlFor="fah">
+            Fahrenheit
+          </label>
+          <input
+            type="string"
+            id="fah"
+            name="fah"
+            value={fahrenheit.toFixed(2) + "°F"}
+            readOnly
+          />
+
+          <input type="submit" value="Convert" />
         </fieldset>
-      </form>
+      </Form>
     </Page>
   );
 }
